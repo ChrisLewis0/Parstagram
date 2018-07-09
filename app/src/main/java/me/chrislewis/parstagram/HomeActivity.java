8d5f4950a49b655041bc97cca0fa1036950fc7ec
Christopher Lewis <cwlewis@fb.com>
2018-07-09 16:10:00 -0700

    Adds Query Subclass to Post
    Adds more functionality to post requests

--- app/src/main/java/me/chrislewis/parstagram/HomeActivity.java
+++ app/src/main/java/me/chrislewis/parstagram/HomeActivity.java
@@ -6,7 +6,6 @@ import android.util.Log;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
-import com.parse.ParseQuery;
 
 import java.util.List;
 
@@ -19,12 +18,17 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        ParseQuery.getQuery(Post.class).findInBackground(new FindCallback<Post>() {
+        final Post.Query postQuery = new Post.Query();
+        postQuery.getTop().withUser();
+
+        postQuery.findInBackground(new FindCallback<Post>() {
             @Override
             public void done(List<Post> objects, ParseException e) {
                 if (e == null) {
                     for(int i = 0; i < objects.size(); i++) {
-                        Log.d("HomeActivity", "Post [" + i + "] = " + objects.get(i).getDescription());
+                        Log.d("HomeActivity", "Post [" + i + "] = "
+                                + objects.get(i).getDescription()
+                                + "username = " + objects.get(i).getUser().getUsername());
                     }
                 }
                 else {
