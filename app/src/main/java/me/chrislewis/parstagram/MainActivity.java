df3c1c80ee69708c4f6844cd2abce19291c705a0
Christopher Lewis <cwlewis@fb.com>
2018-07-09 14:19:05 -0700

    Adds framework for Login

--- app/src/main/java/me/chrislewis/parstagram/MainActivity.java
+++ app/src/main/java/me/chrislewis/parstagram/MainActivity.java
@@ -1,23 +1,55 @@
 package me.chrislewis.parstagram;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText etUsername;
-    EditText etPassword;
-    Button bLogin;
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button bLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        bLogin = findViewById(R.id.bLogin);
 
+        bLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String username = etUsername.getText().toString();
+                final String password = etPassword.getText().toString();
+                login(username, password);
+            }
+        });
+
+    }
+
+    private void login(String username, String password) {
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e != null) {
+                    Log.d("Login Activity", "Login Successful");
+                }
+                else {
+                    Log.d("Login Activity", "Login Failure");
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/me/chrislewis/parstagram/ParseApp.java
@@ -0,0 +1,4 @@
+package me.chrislewis.parstagram;
+
+public class ParseApp {
+}
