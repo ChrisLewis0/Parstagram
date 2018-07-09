2c0c30bb129d109cb19a5b0523417d038dd25f90
Christopher Lewis <cwlewis@fb.com>
2018-07-09 15:34:37 -0700

    Enables Post Information
    In Home Activity, post information can be shown in debugger.

--- app/src/main/java/me/chrislewis/parstagram/ParseApp.java
+++ app/src/main/java/me/chrislewis/parstagram/ParseApp.java
@@ -3,12 +3,17 @@ package me.chrislewis.parstagram;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
+
+import me.chrislewis.parstagram.models.Post;
 
 public class ParseApp extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        ParseObject.registerSubclass(Post.class);
 
         final Parse.Configuration configuration = new Parse.Configuration.Builder(this)
                 .applicationId("BittyMo")
